e83dc1e349401e9d294ce413d92fb5342a09178a
ManuelTello <93638568+ManuelTello@users.noreply.github.com>
2022-10-22 17:04:25 -0300

    Dependecias para testeo unitario agregadas

---
+++ test/fetch.books.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, test } from "@jest/globals";
+
+function suma(a: number, b: number): number {
+    return a + b;
+}
+
+test("lol", () => {
+    expect(2 + 2).toBe(4);
+});
